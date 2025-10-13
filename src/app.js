5d5da17ba9b03c8a52761fb3fd7ffc5b388ee3da
Sanket <sanketgupta1000@gmail.com>
2025-10-13 16:56:14 +0530

    bad-change

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
+// bad change, tests will fail
 app.get('/hello', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!!!!')
 })
 
 // add a new endpoint to see if workflow runs or not
