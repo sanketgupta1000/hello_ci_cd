5b8781dd8974270d77355152314b2562a932fd54
Sanket <sanketgupta1000@gmail.com>
2025-10-13 16:36:20 +0530

    new endpoint

--- src/app.js
+++ src/app.js
@@ -5,4 +5,10 @@ app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
+// let's try to add an endpoint to check if the workflow runs after pull request
+app.get('/hellocicd', (req, res)=>
+{
+    res.send("Hello CI/CD!");
+});
+
 module.exports = app;
