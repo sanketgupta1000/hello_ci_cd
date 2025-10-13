14129ff4e43f1547170f53f8e1415d5a58d27c8e
Sanket <sanketgupta1000@gmail.com>
2025-10-13 16:46:51 +0530

    new endpoint

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
+// new endpoint to check if workflow runs or not
+app.get('/hellocicd', (req, res) => {
+    res.send('Hello CI/CD!')
+})
+
 module.exports = app;
