93ec59c25c38daf8b5e18d42596d20860c357b57
Sanket <sanketgupta1000@gmail.com>
2025-10-13 14:29:32 +0530

    Added first endpoint with tests

---
+++ tests/app.test.js
@@ -0,0 +1,12 @@
+const app = require('../src/app')
+const request = require('supertest');
+
+describe('GET /hello', ()=>
+{
+    it('should return "Hello World!"', async()=>
+    {
+        const res = await request(app).get('/hello');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toEqual("Hello World!");
+    });
+});
